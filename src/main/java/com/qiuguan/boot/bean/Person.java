7964349d487554419bdfcbf8fcc4d5c9f3e19f39
qiuguan <18368116334@163.com>
2022-09-14 01:12:48 +0800

    update bean

--- src/main/java/com/qiuguan/boot/bean/Person.java
+++ src/main/java/com/qiuguan/boot/bean/Person.java
@@ -1,9 +1,17 @@
 package com.qiuguan.boot.bean;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
 /**
  * @author qiuguan
  * @date 2022/09/08 18:54:15  星期四
  */
+@Setter
+@Getter
 public class Person {
 
     private Integer id;
@@ -16,58 +24,22 @@ public class Person {
 
     private String phone;
 
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private Date birthDate;
+
     public Person() {
     }
 
-    public Person(Integer id, String name, String gender, String idCard, String phone) {
+    public Person(Integer id, String name, String gender, String idCard, String phone, Date birthDate) {
         this.id = id;
         this.name = name;
         this.gender = gender;
         this.idCard = idCard;
         this.phone = phone;
+        this.birthDate = birthDate;
     }
 
     public static Person getDefaultBean(){
-        return new Person(1, "秋官", "男", "150422199112095117", "18368116334");
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getIdCard() {
-        return idCard;
-    }
-
-    public void setIdCard(String idCard) {
-        this.idCard = idCard;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
+        return new Person(1, "秋官", "男", "150422199112095117", "18368116334", new Date());
     }
 }
