20437372365fb7ca9380fed12f3c692047f63903
qiuguan <18368116334@163.com>
2022-09-25 15:33:20 +0800

    完成自定义参数解析器-添加到配置类中

--- src/main/java/com/qiuguan/boot/bean/Teacher.java
+++ src/main/java/com/qiuguan/boot/bean/Teacher.java
@@ -1,5 +1,6 @@
 package com.qiuguan.boot.bean;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.util.Date;
@@ -17,5 +18,6 @@ public class Teacher {
 
     private Double salary;
 
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private Date birthDate;
 }
--- src/main/java/com/qiuguan/boot/config/MvcConfig.java
+++ src/main/java/com/qiuguan/boot/config/MvcConfig.java
@@ -2,11 +2,13 @@ package com.qiuguan.boot.config;
 
 import com.qiuguan.boot.convert.IntToEnumConvertFactory;
 import com.qiuguan.boot.handler.UniformResponseHandler;
-import com.qiuguan.boot.convert.EventEnum;
+import com.qiuguan.boot.resolver.RequestBodyMappingHandlerMethodParamResolver;
+import com.qiuguan.boot.resolver.RequestParamMappingAgreementResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
@@ -75,4 +77,10 @@ public class MvcConfig extends WebMvcConfigurationSupport {
 
     }
 
+
+    @Override
+    protected void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(new RequestParamMappingAgreementResolver());
+        argumentResolvers.add(new RequestBodyMappingHandlerMethodParamResolver());
+    }
 }
